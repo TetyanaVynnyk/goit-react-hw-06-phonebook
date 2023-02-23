4a1468b40f8c74fda3075c69b369dbd4cf5fb1b7
Tetyana Vynnyk <kharchenko1988@bigmir.net>
2023-02-23 16:57:51 +0200

    Redone

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from './redux/store';
-
 import Phonebook from 'components/Phonebook/Phonebook';
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div>
-          <Phonebook />
-        </div>
-      </PersistGate>
-    </Provider>
+    <div>
+      <Phonebook />
+    </div>
   );
 };
